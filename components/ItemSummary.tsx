a83da408487a3717f2256b9836c0bb9ebcd05f69
Sean Collins <sean.collins@outlook.com>
2023-05-25 16:26:17 -0700

    Add some Tailwind styling to clean up list view

--- components/ItemSummary.tsx
+++ components/ItemSummary.tsx
@@ -8,7 +8,7 @@ export function ItemSummary(props: ItemSummaryProps) {
   const { item } = props;
 
   return (
-    <>
+    <div class="bg-white w-full max-w-screen-lg py-2 px-10 flex flex-col">
       <div>
         <span>
           <a href={item.url}>{item.title}</a> ({item.url})
@@ -19,6 +19,6 @@ export function ItemSummary(props: ItemSummaryProps) {
           {item.points} point(s) by {item.user} | {item.time} | {item.comments_count} comment(s)
         </span>
       </div>
-    </>
+    </div>
   )
 }
