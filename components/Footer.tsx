ab80dc83ec3b793df064b384dfe590dcc8694afb
Sean Collins <sean.collins@outlook.com>
2023-05-25 12:39:49 -0700

    Build basic footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+export function Footer() {
+  return (
+    <footer>
+      <div>
+        <p>
+          Built with <a href="https://fresh.deno.dev/">Fresh</a> and <a href="https://deno.com/">Deno</a>
+        </p>
+      </div>
+    </footer>
+  )
+}
