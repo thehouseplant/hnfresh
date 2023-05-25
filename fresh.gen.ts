ee5f1f3e31625e54103ae1014c9367bd1298d9ec
Sean Collins <sean.collins@outlook.com>
2023-05-25 16:31:45 -0700

    Add remaining page routes

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -6,14 +6,22 @@ import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/_404.tsx";
 import * as $1 from "./routes/_500.tsx";
 import * as $2 from "./routes/_app.tsx";
-import * as $3 from "./routes/index.tsx";
+import * as $3 from "./routes/ask.tsx";
+import * as $4 from "./routes/index.tsx";
+import * as $5 from "./routes/jobs.tsx";
+import * as $6 from "./routes/new.tsx";
+import * as $7 from "./routes/show.tsx";
 
 const manifest = {
   routes: {
     "./routes/_404.tsx": $0,
     "./routes/_500.tsx": $1,
     "./routes/_app.tsx": $2,
-    "./routes/index.tsx": $3,
+    "./routes/ask.tsx": $3,
+    "./routes/index.tsx": $4,
+    "./routes/jobs.tsx": $5,
+    "./routes/new.tsx": $6,
+    "./routes/show.tsx": $7,
   },
   islands: {},
   baseUrl: import.meta.url,
