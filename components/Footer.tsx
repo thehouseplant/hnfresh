c6b745673fa322a675b6e48b3ed7d86226685a6f
Sean Collins <sean.collins@outlook.com>
2023-05-25 16:12:21 -0700

    Add footer styling care of Fresh components

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,8 @@
 export function Footer() {
   return (
-    <footer>
+    <footer class="bg-white w-full max-w-screen-lg py-6 px-8 flex flex-col md:flex-row gap-4">
       <div>
-        <p>
-          Built with <a href="https://fresh.deno.dev/">Fresh</a> and <a href="https://deno.com/">Deno</a>
-        </p>
+        Built with <a href="https://fresh.deno.dev/">Fresh</a> and <a href="https://deno.com/">Deno</a>
       </div>
     </footer>
   )
