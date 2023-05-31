a0ac5d39df5c16a7f81e4529706e8e6b76835c47
Sean Collins <sean.collins@outlook.com>
2023-05-31 16:35:35 -0700

    Add basic dark mode styles

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -1,10 +1,15 @@
+import { Footer } from "../components/Footer.tsx";
+
 export default function NotFoundPage() {
   return (
-    <div>
-      <h1>404 Not Found</h1>
-      <p>
-        <a href="/">Return to HN Fresh</a>
-      </p>
-    </div>
+    <>
+      <div class="bg-white w-full py-2 px-10 flex flex-col dark:(bg-gray-900 text-gray-300)">
+        <h1>404 Not Found</h1>
+        <p>
+          <a href="/">Return to HN Fresh</a>
+        </p>
+      </div>
+      <Footer />
+    </>
   )
 }
--- routes/_500.tsx
+++ routes/_500.tsx
@@ -1,10 +1,15 @@
+import { Footer } from "../components/Footer.tsx";
+
 export default function NotFoundPage() {
   return (
-    <div>
-      <h1>500 Internal Error</h1>
-      <p>
-        <a href="/">Return to HN Fresh</a>
-      </p>
-    </div>
+    <>
+      <div class="bg-white w-full py-2 px-10 flex flex-col dark:(bg-gray-900 text-gray-300)">
+        <h1>500 Internal Error</h1>
+        <p>
+          <a href="/">Return to HN Fresh</a>
+        </p>
+      </div>
+      <Footer />
+    </>
   )
 }
